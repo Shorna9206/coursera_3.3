7195d63fa7060fe3ff454ca24563b5c760553aa0
shorna9206 <rokeyashorna5@gmail.com>
2020-05-31 01:28:56 +0600

    Coursera Assignment 3.3

---
+++ coursera_3.3.py
@@ -0,0 +1,14 @@
+score = input("Enter Score: ")
+s = float(score)
+if s > 1.0 or s < 0.0:
+    print("Something Error here")
+elif s >= 0.9:
+    print("A")
+elif s >= 0.8:
+    print("B")
+elif s >= 0.7:
+    print("C")
+elif s >= 0.6:
+    print("D")
+else:
+    print("F")
